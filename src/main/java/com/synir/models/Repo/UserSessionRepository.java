4dc2bac7717fed895b2e4572fd338c52f08aa27e
Yichao Qin <31835513+clarenceqy@users.noreply.github.com>
2021-07-19 10:02:04 +0800

    Delete src/main directory

--- src/main/java/com/synir/models/Repo/UserSessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.synir.models.Repo;
-
-import com.synir.models.POJO.UserSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserSessionRepository extends JpaRepository<UserSession, Integer> {
-    UserSession findUsersByUsernameAndPassword(String username, String password);
-    UserSession findUsersByUsername(String username);
-}
